6e58ab067063df65266a92ce4ec5434982799f77
MKNZ_LMNOP <ingridastermoss@gmail.com>
2025-07-23 09:38:35 -0500

    Messing around with SQL and Pandas today

--- gui.py
+++ gui.py
@@ -1,17 +1,24 @@
 import tkinter as tk
+from fridge import add_grocery_item, remove_grocery_item, list_grocery_items
 
-root = tk.Tk()
+window = tk.Tk()
 
-root.geometry("500x500")
-root.title("Fridgeratr")
+def search_fridge():
+    add_grocery_item()
 
-label = tk.Label(root, text="Fridgeratr", font=('Arial', 20))
+window.geometry("500x500")
+window.title("Fridgeratr")
+
+label = tk.Label(window, text="Fridgeratr", font=('Arial', 20))
 label.pack()
 
-entry = tk.Entry(root)
+textboxprompt = tk.Label(window, text="What do you have in your Fridgeratr?", font=('Arial', 10))
+textboxprompt.pack(pady=5)
+
+entry = tk.Entry(window)
 entry.pack(pady=10)
 
-button = tk.Button(root, text="Submit", font=('Arial', 10))
+button = tk.Button(window, text="Submit", font=('Arial', 10), command=search_fridge)
 button.pack(pady=10)
 
-root.mainloop()
+window.mainloop()
